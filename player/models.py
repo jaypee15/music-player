ff80f5cd5e6b6615f63c7f9ccc9061b696a42068
jaypee15 <okoyejohnpaul15@gmail.com>
2023-12-29 07:03:58 +0100

    feat: add models to player app

--- player/models.py
+++ player/models.py
@@ -1,27 +1,74 @@
-from __future__ import unicode_literals
-
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Artist(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    image = models.ImageField(upload_to="artist_image/")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
 class Album(models.Model):
-	artist = models.CharField(max_length=250)
-	album_title = models.CharField(max_length=500)
-	genre = models.CharField(max_length=100)
-	album_logo = models.URLField(max_length=1000)
+    artist_id = models.ForeignKey(
+        Artist, on_delete=models.CASCADE, related_name="albums"
+    )
+    name = models.CharField(max_length=500)
+    cover = models.ImageField(upload_to="album_cover/")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-	def get_absolute_url(self):
-		return reverse('detail', args=[str(self.id)])
+    def get_absolute_url(self):
+        return reverse("detail", args=[str(self.id)])
 
-	def __str__(self):
-		return self.album_title + ' - ' + self.artist
+    def __str__(self):
+        return self.name + " - " + self.artist_id
+
 
 class Song(models.Model):
-	album = models.ForeignKey(Album, on_delete=models.CASCADE)
-	file_type = models.CharField(max_length=10)
-	song_title = models.CharField(max_length=250)
-	is_favorite = models.BooleanField(default=False)
+    album_id = models.ForeignKey(Album, on_delete=models.SET_NULL, null=True)
+    artist_id = models.ForeignKey(
+        Artist, on_delete=models.CASCADE, related_name="songs"
+    )
+    title = models.CharField(max_length=50)
+    length = models.FloatField(max_length=250)
+    lyrics = models.TextField(blank=True)
+    audio_file = models.FileField(upload_to="audio/")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-	def __str__(self):
-		return self.song_title
-	
+    def __str__(self):
+        return self.title
+
+
+class Playlist(models.Model):
+    user_id = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="playlists"
+    )
+    name = models.CharField(max_length=250)
+    image = models.ImageField(upload_to="playlist_covers/")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class PlaylistSong(models.Model):
+    playlist_id = models.ForeignKey(
+        Playlist, on_delete=models.CASCADE, related_name="play_list_songs"
+    )
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
+
+
+class Interaction(models.Model):
+    user_id = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="user_interactions"
+    )
+    song_id = models.ForeignKey(
+        Song, on_delete=models.CASCADE, related_name="song_interactions"
+    )
+    liked = models.BooleanField(default=False)
+    play_count = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
