f690848f3abf95d27edc4477cbd8b5f8f261bd00
jaypee15 <okoyejohnpaul15@gmail.com>
2024-01-31 03:42:10 +0100

    feat: setup static files

--- player/urls.py
+++ player/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("music/", views.IndexView.as_view(), name="index" ),
-    path("music/<int:pk>", views.DetailView.as_view(), name="detail"),
-    path("music/album/add", views.AlbumCreateView.as_view(), name="album-add")
+    path("", views.index, name="index" ),
+  
 ]
--- player/views.py
+++ player/views.py
@@ -1,22 +1,4 @@
-from typing import Any
-from django.db.models.query import QuerySet
-from django.views import generic
-from .models import Album
+from django.shortcuts import render
 
-
-class IndexView(generic.ListView):
-    template_name = "index.html"
-    context_object_name = "all_albums"
-
-    def get_queryset(self) -> QuerySet[Any]:
-        return Album.objects.all()
-
-
-class DetailView(generic.DetailView):
-    model = Album
-    template_name = "detail.html"
-
-
-class AlbumCreateView(generic.CreateView):
-    model = Album
-    fields = ["artist", "album_title", "genre", "album_logo"]
+def index(request):
+    return render(request, 'index.html')
